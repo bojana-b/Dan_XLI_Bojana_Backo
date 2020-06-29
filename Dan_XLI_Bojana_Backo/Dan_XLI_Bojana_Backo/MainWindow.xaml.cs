60fea7ed1c906a96af74163eef506bdb99eee82e
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-06-29 13:20:49 +0200

    Add MainWindow.xaml.cs

--- Dan_XLI_Bojana_Backo/Dan_XLI_Bojana_Backo/MainWindow.xaml.cs
+++ Dan_XLI_Bojana_Backo/Dan_XLI_Bojana_Backo/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,58 @@ namespace Dan_XLI_Bojana_Backo
     /// </summary>
     public partial class MainWindow : Window
     {
+        static BackgroundWorker _bw;
         public MainWindow()
         {
+            _bw = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true
+            };
+
             InitializeComponent();
+        }
+        static void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                Thread.Sleep(100);
+                sum = sum + i;
+                // Calling ReportProgress() method raises ProgressChanged event
+                // To this method pass the percentage of processing that is complete
+                _bw.ReportProgress(i);
+
+                // Check if the cancellation is requested
+                if (_bw.CancellationPending)
+                {
+                    // Set Cancel property of DoWorkEventArgs object to true
+                    e.Cancel = true;
+                    // Reset progress percentage to ZERO and return
+                    _bw.ReportProgress(0);
+                    return;
+                }
+            }
+        }
+        static void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //progressBar.Value 
+            
+            
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
         }
     }
 }
